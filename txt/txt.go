4fc20abd530796e49e76cc196d4ff38a0c8be7d9
Harsh K <hkundra@gmail.com>
2018-03-07 11:30:16 +0530

    initial commit

---
+++ txt/txt.go
@@ -0,0 +1,38 @@
+package txt
+
+import (
+	"strings"
+	"unicode"
+
+	"github.com/azer/snakecase"
+)
+
+func CaseSnake(text string) string {
+	return snakecase.SnakeCase(text)
+}
+
+func CaseURL(text string) string {
+	var words []string
+	l := 0
+	for s := text; s != ""; s = s[l:] {
+		l = strings.IndexFunc(s[1:], unicode.IsUpper) + 1
+		if l <= 0 {
+			l = len(s)
+		}
+		words = append(words, s[:l])
+	}
+
+	url := strings.ToLower(strings.Join(words, "-"))
+	url = strings.Replace(url, "--", "-", -1) // pure hack. todo: reg-ex
+	return url
+}
+
+func CaseSentence(text string) string {
+	if len(text) > 0 {
+		u := []rune(text)
+		u[0] = unicode.ToUpper(u[0])
+		return string(u)
+	}
+
+	return text
+}
